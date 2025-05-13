943e70a580f22293068e5585165481c4a169ab0c
palani-selvam <palaniselvam.m@adjecti.com>
2025-05-13 15:55:49 +0530

    What are Included:  - Deleted serializers and views in core of group and user

--- bridgesec_data_transformer/core/serializers/group_serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class GroupSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    description = serializers.CharField()
-    custom_profile_attributes = serializers.DictField(required=False)
