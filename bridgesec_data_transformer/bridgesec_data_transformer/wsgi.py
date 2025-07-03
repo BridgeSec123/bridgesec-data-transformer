f92b092992e9057c60b4b17e9df3e3a65388f7f9
palani-selvam <palaniselvam.m@adjecti.com>
2025-07-03 20:32:56 +0530

    Added python path in wsgi

--- bridgesec_data_transformer/bridgesec_data_transformer/wsgi.py
+++ bridgesec_data_transformer/bridgesec_data_transformer/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bridgesec_data_transformer.bridgesec_data_transformer.settings')
 
