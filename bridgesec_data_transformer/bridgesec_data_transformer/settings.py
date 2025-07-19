17a2fa2b9ab0086611b8660822cb052f551368db
palani-selvam <palaniselvam.m@adjecti.com>
2025-07-19 20:54:26 +0530

    Changes in static files

--- bridgesec_data_transformer/bridgesec_data_transformer/settings.py
+++ bridgesec_data_transformer/bridgesec_data_transformer/settings.py
@@ -141,7 +141,7 @@ SESSION_COOKIE_SAMESITE = "Lax"
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
