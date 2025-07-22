9edf284b5c489f58f4b37b0f3adce2dd4d28ba7a
palani-selvam <palaniselvam.m@adjecti.com>
2025-07-22 14:53:15 +0530

    Changes in entrypoint.sh for Deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-echo "Applying migrations..."
+#!/bin/sh
+
+echo "Running DB migrations..."
 python manage.py migrate
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-exec "$@"
+echo "Starting Django with debugger..."
+python -m debugpy --listen 0.0.0.0:5678 --wait-for-client manage.py runserver 0.0.0.0:8000
